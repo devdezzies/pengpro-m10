5771501f927b7095f977b80656ed44380bc27e94
Abdullah <boltholt17@gmail.com>
2023-11-27 15:11:57 +0700

    revisi

--- avatar.go
+++ avatar.go
@@ -14,11 +14,14 @@ func main() {
 		} else {
 			fmt.Println(3, "Dewasa", (orang - 15) / 2, "Kecil")
 		}
-	} else if orang < 15 {
+	} else if orang < 15 && orang != 10 && orang != 5 {
 		if orang % 15 > 0 {
 			fmt.Println(orang / 5 + 1, "Dewasa")
 		} else {
 			fmt.Println(orang / 5, "Dewasa")
 		}
+	} else if orang == 5 || orang == 10 {
+		fmt.Println(orang / 5, "Dewasa")
 	}
 }
+
