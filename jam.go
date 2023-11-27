b5fa6e816906b62a232d8d96d38b553feb5c5fa4
Abdullah <boltholt17@gmail.com>
2023-11-27 16:03:16 +0700

    revised parkir

--- jam.go
+++ jam.go
@@ -5,9 +5,14 @@ import "fmt"
 func main() {
 	var h1, m1, h2, m2 int 
 	var jam, menit int
+	var time int
 
 	fmt.Scan(&h1, &m1, &h2, &m2) 
-	if (h1 < h2) {
+	if (h1 < h2) && (h2 - h1 == 1) {
+		time = (60 - m1) + m2
+        jam = time / 60
+        menit = time % 60
+	} else if (h1 < h2) {
 		jam = h2 - h1
 		menit = m1 - m2
 		if (menit >= 0) {
